a3e261e8dc804cc802fa5fa23bec059f01a2661c
Deepanshu Gupta <guptadeepanshu25@gmail.com>
2025-12-28 13:41:24 +0530

    update db urluse

--- src/db.js
+++ src/db.js
@@ -1,12 +1,14 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'sampleapp',
-  password: process.env.DB_PASSWORD || 'password',
-  port: process.env.DB_PORT || 5432,
-});
+const pool = new Pool(
+  process.env.DB_URL || {
+    user: process.env.DB_USER || 'postgres',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'sampleapp',
+    password: process.env.DB_PASSWORD || 'password',
+    port: process.env.DB_PORT || 5432,
+  }
+);
 
 const initDB = async () => {
   try {
